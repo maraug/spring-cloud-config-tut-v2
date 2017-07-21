72910b742ad0cd7cc90f194bc7bf1def55e7a0e4
mrcs <marcin.aug@gmail.com>
2017-07-21 13:41:25 +0200

    Initial commit

---
+++ src/test/java/com/mrcs/CloudTutConfigServerV2ApplicationTests.java
@@ -0,0 +1,16 @@
+package com.mrcs;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CloudTutConfigServerV2ApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
